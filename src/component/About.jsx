7ace646ad446c964a7b69ce9c0c6596155cceccd
ZenDev <ozenandreiii@gmail.com>
2024-09-30 22:11:09 +0800

    edited about content

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -1,22 +1,27 @@
 import React from "react";
 import { motion as m } from "framer-motion";
 import { GoGoal } from "react-icons/go";
+import { RxAvatar } from "react-icons/rx";
+import { GrTrophy } from "react-icons/gr";
 
 const sections = [
   {
     title: "Self Introduction",
+    icon: <RxAvatar className="text-lightModeTextT dark:text-darkModeTextP" />,
     content:
-      "I’m a recent graduate passionate about web development, aiming to grow as a junior front-end developer. With a focus on React and modern web technologies, my goal is to build dynamic, user-friendly websites while continuously learning and improving my skills.",
+      "I’m passionate about web development, with a focus on React and modern technologies. I strive to build dynamic, user-friendly websites and spend 6 to 8 hours daily coding, taking on new challenges and exploring solutions. This dedication drives me to continuously learn and improve, bringing me closer to my goals in the ever-evolving tech landscape.",
   },
   {
-    title: "Self Introduction",
+    title: "Short Term Goal",
+    icon: <GoGoal className="text-lightModeTextT dark:text-darkModeTextP" />,
     content:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur omnis a facere voluptatem quod accusantium fuga nesciunt eius ea corporis ipsum pariatur, quaerat optio vero eum repudiandae nihil. Consequatur animi provident aperiam, dolores quam quae inventore minima impedit, ratione sunt soluta veniam atque tempora",
+      "My short-term goal is to secure a position as a junior front-end developer, where I can apply my knowledge and enthusiasm in a professional setting. I am eager to contribute to a collaborative team, engage in exciting projects, and gain valuable hands-on experience. This opportunity will help me further develop my skills in building responsive and engaging web applications while staying updated with industry trends and best practices.",
   },
   {
-    title: "Self Introduction",
+    title: "Long Term Plan",
+    icon: <GrTrophy className="text-lightModeTextT dark:text-darkModeTextP" />,
     content:
-      "I’m a recent graduate passionate about web development, aiming to grow as a junior front-end developer. With a focus on React and modern web technologies, my goal is to build dynamic, user-friendly websites while continuously learning and improving my skills.",
+      "My long-term goal is to become proficient in full-stack development, mastering both front-end and back-end technologies. I aspire to become a tech lead, guiding young developers and helping them succeed. I also dream of starting my own tech company, where I can innovate, solve real-world problems, and foster a collaborative and growth-focused culture.",
   },
 ];
 
@@ -48,7 +53,7 @@ export default function About() {
 
       <div className="flex flex-col md:flex-row justify-center px-8  gap-6 max-w-[1000px] mx-auto">
         {sections.map((section, index) => (
-          <div key={index} className="relative overflow-hidden space-y-4">
+          <div key={index} className="relative overflow-hidden space-y-4 md:flex-1">
             <m.div
               initial={{
                 opacity: 0.5,
@@ -64,7 +69,7 @@ export default function About() {
               }}
               className="flex items-center gap-2 "
             >
-              <GoGoal className="text-lightModeTextT dark:text-darkModeTextP" />
+              {section.icon}
               <h1 className="text-lightModeTextT dark:text-darkModeTextP">
                 {section.title}
               </h1>
