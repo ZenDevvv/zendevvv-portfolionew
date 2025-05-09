3abbbd2a3245b8eeea7231389798dc541749d069
ZenDevvv <ozenandreiiii@gmail.com>
2025-05-09 13:27:43 +0800

    add experiences data and refactor Experience component for dynamic rendering

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -17,7 +17,10 @@ export default function Contact() {
   ];
 
   return (
-    <section className="bg-secondaryLightBG dark:bg-secondaryDarkBG overflow-hidden py-12" id="contact">
+    <section
+      className="bg-secondaryLightBG dark:bg-secondaryDarkBG overflow-hidden py-12"
+      id="contact"
+    >
       <div className="grid place-items-center">
         <h1 className="inline-block text-xs px-3 py-1.5 rounded-full bg-gray-300 text-center text-lightModeTextP dark:text-darkModeTextP dark:bg-gray-600 mb-8">
           Get in touch
@@ -29,8 +32,9 @@ export default function Contact() {
         <m.div
           initial={{ opacity: 0, y: 30 }}
           whileInView={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.7, ease: "easeOut"}}
-          className="overflow-hidden text-center flex flex-row items-center gap-2 mt-6">
+          transition={{ duration: 0.7, ease: "easeOut" }}
+          className="overflow-hidden text-center flex flex-row items-center gap-2 mt-6"
+        >
           <MdOutlineEmail className="text-xl text-lightModeTextT dark:text-darkModeTextT" />{" "}
           <a
             className="text-lg text-lightModeTextT dark:text-darkModeTextT"
@@ -42,8 +46,9 @@ export default function Contact() {
         <m.div
           initial={{ opacity: 0, y: 30 }}
           whileInView={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.7, ease: "easeOut", delay: 0.2}}
-          className="text-center flex flex-row items-center gap-2 mb-6">
+          transition={{ duration: 0.7, ease: "easeOut", delay: 0.2 }}
+          className="text-center flex flex-row items-center gap-2 mb-6"
+        >
           <FiPhone className="text-xl text-lightModeTextT dark:text-darkModeTextT" />{" "}
           <p className="text-lg text-lightModeTextT dark:text-darkModeTextT">
             +63 906 857 5015
@@ -51,21 +56,36 @@ export default function Contact() {
         </m.div>
 
         <div className="flex flex-col items-center gap-4">
-          <m.p 
+          <m.p
             initial={{ opacity: 0, y: 30 }}
             whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.7, ease: "easeOut", delay: 0.2}}
-            className="text-sm text-lightModeTextP dark:text-darkModeTextP">
+            transition={{ duration: 0.7, ease: "easeOut", delay: 0.2 }}
+            className="text-sm text-lightModeTextP dark:text-darkModeTextP"
+          >
             You may also find me on these platforms!
           </m.p>
           <div className="flex flex-row items-center gap-3">
             {icons.map((icon, index) => (
               <m.a
                 initial={{ opacity: 0, y: 30 }}
-                
-                whileInView={{ opacity: 1, y: 0, transition: { duration: 0.7, ease: "easeOut", delay: index * 0.15 + 0.4}}}
-                whileHover={{ scale: 1.3, transition: { duration: 0.15, ease: "easeInOut" }}}
-                key={index} href={icon.href} target="_blank" rel="noopener noreferrer">
+                whileInView={{
+                  opacity: 1,
+                  y: 0,
+                  transition: {
+                    duration: 0.7,
+                    ease: "easeOut",
+                    delay: index * 0.15 + 0.4,
+                  },
+                }}
+                whileHover={{
+                  scale: 1.3,
+                  transition: { duration: 0.15, ease: "easeInOut" },
+                }}
+                key={index}
+                href={icon.href}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <icon.icon className="text-lg text-lightModeTextT dark:text-darkModeTextT" />
               </m.a>
             ))}
--- src/component/Experience.jsx
+++ src/component/Experience.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion as m } from "framer-motion";
-
+import { experiences } from "../assets/json/experiences.json";
 
 const IPSolutions = () => {
   return (
@@ -11,28 +11,11 @@ const IPSolutions = () => {
 };
 
 export default function Experience() {
-  const experiences = [
-    {
-      company: "Hire me? 👉🏻👈🏻",
-      period: "present",
-      position: "Junior frontend developer",
-      tasks: [
-        "Waiting for your offer letter",
-      ],
-    },
-    {
-      company: "IPSolutions Inc.",
-      period: "March 2024 - May 2024",
-      position: "IT Intern",
-      tasks: [
-        "Developed static websites utilizing HTML/CSS and Bootstrap templates, following UI/UX design principles to ensure responsive and user-friendly layouts.",
-        "Collaborated with cross-functional teams to ensure the development process adhered to Software Development Life Cycle (SDLC) practices.",
-      ],
-    },
-  ];
-
   return (
-    <section className="bg-secondaryLightBG dark:bg-secondaryDarkBG overflow-hidden py-12" id="experience">
+    <section
+      className="bg-secondaryLightBG dark:bg-secondaryDarkBG overflow-hidden py-12"
+      id="experience"
+    >
       <div className="max-w-screen-lg mx-auto px-4">
         <div className="grid place-items-center pb-6">
           <h1 className="inline-block text-xs px-3 py-1.5 rounded-full bg-gray-300 text-center text-lightModeTextP dark:text-darkModeTextP dark:bg-gray-600">
@@ -41,42 +24,57 @@ export default function Experience() {
         </div>
 
         <div className="flex flex-col gap-4">
-          {experiences.map((exp, index) => (
-            <m.div
-              initial={{
-                opacity: 0,
-                x: '50%',
-              }}
-              whileInView={{
-                opacity: 1,
-                x: 0,
-              }}
-              transition={{
-                duration: 1,
-                ease: "easeInOut",
-              }}
-              key={index}
-              className="flex flex-col gap-2 justify-between bg-lightBG shadow-lg dark:bg-gray-800 rounded-2xl p-8"
-            >
-              <div className="flex flex-col gap-4 md:flex-row md:justify-between md:items-center">
-                <h1 className="text-xl font-bold text-lightModeTextT dark:text-darkModeTextT md:w-1/4 md:flex-initial">
-                  {exp.company === "IPSolutions Inc." ? <IPSolutions /> : exp.company}
-                </h1>
-                <p className="text-xs md:text-right text-lightModeTextP dark:text-darkModeTextP md:order-2 md:w-1/4 md:flex-initial">
+          <div className="flex flex-col gap-4">
+            {experiences.map((exp, index) => (
+              <m.div
+                initial={{
+                  opacity: 0,
+                  x: "50%",
+                }}
+                whileInView={{
+                  opacity: 1,
+                  x: 0,
+                }}
+                transition={{
+                  duration: 1,
+                  ease: "easeInOut",
+                }}
+                key={index}
+                className="grid grid-cols-1 md:grid-cols-4 gap-4 bg-lightBG shadow-lg dark:bg-gray-800 rounded-2xl p-8"
+              >
+                {/* Company */}
+                <div className="flex flex-col gap-2 md:col-span-1">
+                  <h1 className="text-xl font-bold text-lightModeTextT dark:text-darkModeTextT">
+                    {exp.company === "IPSolutions Inc." ? (
+                      <IPSolutions />
+                    ) : (
+                      exp.company
+                    )}
+                  </h1>
+                  <p className="text-xs text-lightModeTextP dark:text-darkModeTextP md:hidden">
+                    {exp.period}
+                  </p>
+                </div>
+
+                {/* Position and Tasks */}
+                <div className="flex flex-col gap-4 md:col-span-2 md:order-2">
+                  <h2 className="font-bold text-lightModeTextT dark:text-darkModeTextT text-xl">
+                    {exp.position}
+                  </h2>
+                  <ul className="list-disc text-lightModeTextP dark:text-darkModeTextP pl-6 space-y-2">
+                    {exp.tasks.map((task, i) => (
+                      <li key={i}>{task}</li>
+                    ))}
+                  </ul>
+                </div>
+
+                {/* Period - Hidden on mobile */}
+                <p className="hidden md:block text-xs text-lightModeTextP dark:text-darkModeTextP text-center md:order-3">
                   {exp.period}
                 </p>
-                <h2 className="font-bold text-lightModeTextT md:order-1 dark:text-darkModeTextT md:text-xl md:w-1/2 md:flex-1">
-                  {exp.position}
-                </h2>
-              </div>
-
-              <ul className="list-disc text-lightModeTextP dark:text-darkModeTextP pl-6 space-y-2 md:w-1/2 max-w-[400px] mx-auto">
-                {exp.tasks.map((task, i) => (
-                  <li key={i}>{task}</li>
-                ))}
-              </ul>
-            </m.div>
-          ))}
+              </m.div>
+            ))}
+          </div>
         </div>
       </div>
     </section>
